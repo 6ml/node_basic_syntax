f442a807c3f371e7ec5f46a480143db47a71f707
lupath <849104822@qq.com>
2017-07-21 18:38:50 +0800

    [fix] use COS to save img

--- js/restful_api/planListApi.js
+++ js/restful_api/planListApi.js
@@ -1,12 +1,19 @@
 // create RESTful server
 
 const express = require('express');
-const mongoose = require('../mongodb/mongoose/db');
+const COS = require('cos-nodejs-sdk-v5');
+const multipart = require('connect-multiparty');
+let bodyParser = require('body-parser');
+let fs = require('fs');
 
 let app = express();
 let router = express.Router();
-let bodyParser = require('body-parser');
+// multipartMiddleware 中间件，上传文件用，将 req 中文件内容从 req.body 拆分至 req.files
+let multipartMiddleware = multipart();
+const mongoose = require('../mongodb/mongoose/db');
 let Schema = mongoose.Schema;
+
+const COS_URL = 'http://vue-1251802397.coscd.myqcloud.com/';
 
 // create Mongoose Schema
 // create PlanSchema
@@ -109,24 +116,75 @@ let addPlan = (planObj) => {
 	});
 };
 
-router.post('/addPlan', (req, res) => {
-	console.log(req.body);
-	// get user input
+router.post('/addPlan', multipartMiddleware, (req, res) => {
+	// 获取用户输入
+	let file = req.files.avatar;
+
 	let requestBody = {
 		username: req.body.username,
-		avatar: req.body.avatar,
 		date: req.body.date,
 		totalTime: parseInt(req.body.totalTime),
 		comment: req.body.comment
 	};
 
-	addPlan(requestBody)
-		.then( () => {
-			res.send("添加成功!");
-		})
-		.catch( () => {
-			res.send("添加失败!");
+	console.log(req.body);
+	console.log('------------');
+	console.log(req.files);
+	console.log('===========================\n\n');
+
+	// 使用腾讯云存储存放图片
+	let cos = new COS({
+	    AppId: '1251802397',
+	    SecretId: 'AKID54uZ5SBdR3Wr1zQMlvyeEdqsiMo4GMND',
+	    SecretKey: 'nPp360pUyHWcXGn9KJ7nylYEi060iHj6',
+	});
+
+	var params = {
+	    Bucket : 'vue',
+	    Region : 'cn-southwest',
+	    Key : file.originalFilename,
+	    // CacheControl : 'STRING_VALUE',
+	    // ContentDisposition : 'STRING_VALUE',
+	    // ContentEncoding : 'STRING_VALUE',
+	    ContentLength : file.size,
+	    // ContentType : 'STRING_VALUE',
+	    // Expect : 'STRING_VALUE',
+	    // Expires : 'STRING_VALUE',
+	    // ContentSha1 : 'STRING_VALUE',
+	    // ACL : 'STRING_VALUE',
+	    // GrantRead : 'STRING_VALUE',
+	    // GrantWrite : 'STRING_VALUE',
+	    // GrantFullControl : 'STRING_VALUE',
+	    // 'x-cos-meta-*' : 'STRING_VALUE',
+	    Body: fs.createReadStream(file.path),
+	    onProgress: function (progressData) {
+	        console.log(progressData);
+	    },
+	};
+	
+	// 上传文件至 COS -> addPlan (存数据库) -> 添加成功
+	// 										-x 添加失败
+	// 				  -x 添加失败
+	new Promise((resolve, reject) => {
+		// 向 COS 传文件
+		cos.putObject(params, function(err) {
+		    if(err) {
+		        reject(err);
+		    } else {
+		        file.url = COS_URL + file.originalFilename;
+		    }
+		    resolve(file.url);
 		});
+	}).then((url) => {
+		requestBody.avatar = url;
+		addPlan(requestBody)
+			.then( (result) => {
+				res.send(result);
+			});
+	}).catch( () => {
+		res.send("添加失败!");
+	});
+	
 });
 
 let deletePlan = (id) => {
@@ -143,7 +201,6 @@ let deletePlan = (id) => {
 
 router.post('/deletePlan', (req, res) => {
 	let id = req.body.id;
-	console.log(id);
 
 	deletePlan(id)
 		.then( () => {
